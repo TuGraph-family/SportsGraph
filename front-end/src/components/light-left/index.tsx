1a435c86284f2329533f68f20326bf385728f6e9
Seventysevendays <867789352@qq.com>
2024-06-02 23:12:47 +0800

    fix: fix compete team style

--- front-end/src/components/light-left/index.tsx
+++ front-end/src/components/light-left/index.tsx
@@ -1,91 +1,101 @@
+import "./index.less";
+
 const LightLeft: React.FC = () => {
   return (
-    <svg
-      width="341px"
-      height="341px"
-      viewBox="0 0 341 341"
-      version="1.1"
-      xmlns="http://www.w3.org/2000/svg"
-      xmlnsXlink="http://www.w3.org/1999/xlink"
-    >
-      <title>编组备份</title>
-      <defs>
-        <linearGradient
-          x1="100%"
-          y1="50%"
-          x2="1.54062177e-14%"
-          y2="50%"
-          id="light-left-linearGradient-1"
-        >
-          <stop stop-color="#FFFFFF" offset="0%"></stop>
-          <stop stop-color="#FFFFFF" stop-opacity="0" offset="100%"></stop>
-        </linearGradient>
-        <filter
-          x="-13.7%"
-          y="-21.1%"
-          width="127.5%"
-          height="142.2%"
-          filterUnits="objectBoundingBox"
-          id="light-left-filter-2"
-        >
-          <feGaussianBlur stdDeviation="10" in="SourceGraphic"></feGaussianBlur>
-        </filter>
-        <linearGradient
-          x1="100%"
-          y1="50%"
-          x2="1.54062177e-14%"
-          y2="50%"
-          id="light-left-linearGradient-3"
-        >
-          <stop stop-color="#FFFFFF" offset="0%"></stop>
-          <stop stop-color="#FFFFFF" stop-opacity="0" offset="100%"></stop>
-        </linearGradient>
-        <filter
-          x="-5.7%"
-          y="-37.5%"
-          width="111.4%"
-          height="175.0%"
-          filterUnits="objectBoundingBox"
-          id="light-left-filter-4"
-        >
-          <feGaussianBlur stdDeviation="3" in="SourceGraphic"></feGaussianBlur>
-        </filter>
-      </defs>
-      <g
-        id="light-left-设计稿"
-        stroke="none"
-        stroke-width="1"
-        fill="none"
-        fill-rule="evenodd"
+    <div className="sg-light-left">
+      <svg
+        width="341px"
+        height="341px"
+        viewBox="0 0 341 341"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
       >
+        <title>编组备份</title>
+        <defs>
+          <linearGradient
+            x1="100%"
+            y1="50%"
+            x2="1.54062177e-14%"
+            y2="50%"
+            id="light-left-linearGradient-1"
+          >
+            <stop stop-color="#FFFFFF" offset="0%"></stop>
+            <stop stop-color="#FFFFFF" stop-opacity="0" offset="100%"></stop>
+          </linearGradient>
+          <filter
+            x="-13.7%"
+            y="-21.1%"
+            width="127.5%"
+            height="142.2%"
+            filterUnits="objectBoundingBox"
+            id="light-left-filter-2"
+          >
+            <feGaussianBlur
+              stdDeviation="10"
+              in="SourceGraphic"
+            ></feGaussianBlur>
+          </filter>
+          <linearGradient
+            x1="100%"
+            y1="50%"
+            x2="1.54062177e-14%"
+            y2="50%"
+            id="light-left-linearGradient-3"
+          >
+            <stop stop-color="#FFFFFF" offset="0%"></stop>
+            <stop stop-color="#FFFFFF" stop-opacity="0" offset="100%"></stop>
+          </linearGradient>
+          <filter
+            x="-5.7%"
+            y="-37.5%"
+            width="111.4%"
+            height="175.0%"
+            filterUnits="objectBoundingBox"
+            id="light-left-filter-4"
+          >
+            <feGaussianBlur
+              stdDeviation="3"
+              in="SourceGraphic"
+            ></feGaussianBlur>
+          </filter>
+        </defs>
         <g
-          id="light-left-对抗度"
-          transform="translate(100.000000, -358.000000)"
+          id="light-left-设计稿"
+          stroke="none"
+          stroke-width="1"
+          fill="none"
+          fill-rule="evenodd"
         >
           <g
-            id="light-left-编组备份"
-            transform="translate(70.500296, 528.500296) scale(1, -1) translate(-70.500296, -528.500296) translate(-57.000000, 401.000000)"
+            id="light-left-对抗度"
+            transform="translate(100.000000, -358.000000)"
           >
-            <polygon
-              id="light-left-矩形备份-15"
-              fill="url(#light-left-linearGradient-1)"
-              opacity="0.731770833"
-              filter="url(#light-left-filter-2)"
-              transform="translate(127.500296, 127.500296) scale(-1, -1) rotate(-45.000000) translate(-127.500296, -127.500296) "
-              points="18.304294 56.38365 230.872833 118.571176 236.696297 134.619379 26.052304 198.616941"
-            ></polygon>
-            <polygon
-              id="light-left-矩形备份-15"
-              fill="url(#light-left-linearGradient-3)"
-              opacity="0.504022507"
-              filter="url(#light-left-filter-4)"
-              transform="translate(95.493961, 155.273193) scale(-1, -1) rotate(-45.000000) translate(-95.493961, -155.273193) "
-              points="16.5474446 143.273193 170.241918 149.150842 174.440477 154.957529 23.2294526 167.273193"
-            ></polygon>
+            <g
+              id="light-left-编组备份"
+              transform="translate(70.500296, 528.500296) scale(1, -1) translate(-70.500296, -528.500296) translate(-57.000000, 401.000000)"
+            >
+              <polygon
+                id="light-left-矩形备份-15"
+                fill="url(#light-left-linearGradient-1)"
+                opacity="0.731770833"
+                filter="url(#light-left-filter-2)"
+                transform="translate(127.500296, 127.500296) scale(-1, -1) rotate(-45.000000) translate(-127.500296, -127.500296) "
+                points="18.304294 56.38365 230.872833 118.571176 236.696297 134.619379 26.052304 198.616941"
+              ></polygon>
+              <polygon
+                id="light-left-矩形备份-15"
+                fill="url(#light-left-linearGradient-3)"
+                opacity="0.504022507"
+                filter="url(#light-left-filter-4)"
+                transform="translate(95.493961, 155.273193) scale(-1, -1) rotate(-45.000000) translate(-95.493961, -155.273193) "
+                points="16.5474446 143.273193 170.241918 149.150842 174.440477 154.957529 23.2294526 167.273193"
+              ></polygon>
+            </g>
           </g>
         </g>
-      </g>
-    </svg>
+      </svg>
+    </div>
   );
 };
 
