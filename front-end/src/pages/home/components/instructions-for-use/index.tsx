65c54ec33f7aa90b711be78e058e5b7aba5fbcb0
yuaiai0419 <yay01169903@antgroup.com>
2024-06-04 16:23:25 +0800

    fix: 使用声明，技术原理样式优化

--- front-end/src/pages/home/components/instructions-for-use/index.tsx
+++ front-end/src/pages/home/components/instructions-for-use/index.tsx
@@ -44,20 +44,23 @@ const InstructionsForUse: React.FC<InstructionsForUseProps> = ({}) => {
     <Mask visible={visible} className="instructions-for-use">
       <div className="text-content ">
         <div className="title">使用声明</div>
-        <li>
-          应用展示的所有数据均来源于公开渠道，我们不对这些数据的准确性、可靠性、完整性或时效性作出任何保证。
-        </li>
-        <li>
-          本应用提供的所有分析、计算结果仅限于非商业性、信息提供的目的使用，使用本应用意味着用户同意不将其用于违反任何适用法律、法规的活动。
-        </li>
-        <li>
-          任何用户交互功能仅为增加互动性和娱乐性，与实际比赛无关。我们不承担任何因使用或无法使用本应用所引起的直接或间接损害。
-        </li>
+        <ul className="styled-list">
+          <li>
+            应用展示的所有数据均来源于公开渠道，我们不对这些数据的准确性、可靠性、完整性或时效性作出任何保证。
+          </li>
+          <li>
+            本应用提供的所有分析、计算结果仅限于非商业性、信息提供的目的使用，使用本应用意味着用户同意不将其用于违反任何适用法律、法规的活动。
+          </li>
+          <li>
+            任何用户交互功能仅为增加互动性和娱乐性，与实际比赛无关。我们不承担任何因使用或无法使用本应用所引起的直接或间接损害。
+          </li>
+        </ul>
         <Checkbox block onChange={onChange} className="checkbox">
           我已阅读、理解并接受本使用声明的条款。
         </Checkbox>
         <div className="agree-button">
           <Button
+            isShowHighlightBorder
             onClick={onContinue}
             style={{ padding: "11px 19px 11px 18px" }}
             disabled={!agree}
